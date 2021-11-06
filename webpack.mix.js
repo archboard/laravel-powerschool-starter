ce9de82aae1a3f5276e0a568e4d3b4bffec8ec8c
Grant Holle <hollegrant@gmail.com>
2021-11-06 20:46:02 +0800

    Remove cypress files and update frontend dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -23,7 +23,6 @@ mix.js('resources/js/app.js', 'public/js')
   .postCss('resources/css/app.css', 'public/css', [
     require('postcss-import'),
     require('tailwindcss'),
-    require('postcss-nested'),
   ])
   .babelConfig({
     plugins: ['@babel/plugin-syntax-dynamic-import'],
