f004544d978c60b61a16af0dd9881822369684d5
Grant Holle <hollegrant@gmail.com>
2022-09-26 16:52:52 +0400

    Add smtp fields for tenants

--- database/migrations/2014_03_10_032305_create_tenants_table.php
+++ database/migrations/2014_03_10_032305_create_tenants_table.php
@@ -20,6 +20,13 @@ class CreateTenantsTable extends Migration
             $table->dateTime('subscription_expires_at')->nullable();
             $table->string('license');
             $table->string('sis_provider')->default(\App\Enums\Sis::PS->value);
+            $table->string('smtp_host')->nullable();
+            $table->string('smtp_port')->nullable();
+            $table->string('smtp_username')->nullable();
+            $table->string('smtp_password')->nullable();
+            $table->string('smtp_from_name')->nullable();
+            $table->string('smtp_from_address')->nullable();
+            $table->string('smtp_encryption')->nullable();
             $table->timestamps();
         });
     }
