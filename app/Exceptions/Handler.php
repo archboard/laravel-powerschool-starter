974b6e83d043bc590e93e098bfc86f7d1c015d33
Grant Holle <hollegrant@gmail.com>
2023-04-08 22:48:55 -0500

    Render nice error page in production only

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,7 +44,7 @@ class Handler extends ExceptionHandler
         $response = parent::render($request, $e);
 
         if (
-            !app()->environment('local') &&
+            app()->environment('production') &&
             in_array($response->status(), [500, 503, 404, 403]) &&
             (!$request->wantsJson() || $request->inertia())
         ) {
