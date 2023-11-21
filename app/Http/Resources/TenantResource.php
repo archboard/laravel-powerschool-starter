3413a0bd617fb4a0465203ba8627345e99921afb
Grant Holle <hollegrant@gmail.com>
2023-11-21 13:39:54 -0600

    Improve tenant resource

--- app/Http/Resources/TenantResource.php
+++ app/Http/Resources/TenantResource.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Tenant;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/** @property-read Tenant $resource */
 class TenantResource extends JsonResource
 {
     /**
@@ -15,11 +17,12 @@ class TenantResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'domain' => $this->domain,
-            'license' => $this->license,
-            'subscription_started_at' => $this->subscription_started_at,
-            'allow_oidc_login' => $this->allow_oidc_login,
-            'allow_password_auth' => $this->allow_password_auth,
+            'domain' => $this->resource->domain,
+            'license' => $this->resource->license,
+            'subscription_started_at' => $this->resource->subscription_started_at,
+            'allow_oidc_login' => $this->resource->allow_oidc_login,
+            'allow_password_auth' => $this->resource->allow_password_auth,
+            'sis' => $this->resource->sis_provider->label(),
         ];
     }
 }
