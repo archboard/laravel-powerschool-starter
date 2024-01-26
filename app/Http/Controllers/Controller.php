d8dc7d4f1070945079bd7f5ff99ad0688592161b
Grant Holle <hollegrant@gmail.com>
2024-01-26 13:01:48 +0400

    Overwrite method to allow for enum usage

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Navigation\NavigationItem;
+use Illuminate\Contracts\Auth\Access\Gate;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -15,5 +16,12 @@ class Controller extends BaseController
     protected function withBreadcrumbs(NavigationItem ...$item): array
     {
         return array_map(fn (NavigationItem $item) => $item->toArray(), $item);
+    }
+
+    public function authorize($ability, $arguments = [])
+    {
+        [$ability, $arguments] = $this->parseAbilityAndArguments($ability?->value ?? $ability, $arguments);
+
+        return app(Gate::class)->authorize($ability, $arguments);
     }
 }
